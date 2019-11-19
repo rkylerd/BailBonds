07448e88fbd03872b7fb75675c79ceb6c3ad518d
Kayla Pexton <kaylapex@gmail.com>
2019-11-19 11:22:55 -0700

    idk

---
+++ BailBonds/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BailBonds.Models
+{
+    public class Contact
+    {
+        [Required]
+        [Display(Name = "Name:")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Email:")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Subject:")]
+        public string Subject { get; set; }
+        [Display(Name = "Message Body:")]
+        public string Message { get; set; }
+    }
+public enum Subject
+{
+    FAQ,
+    Agents
+}
+}
